a164efef43c650c0009e83787b1d7e2c383fd309
Daniel Vázquez <zydeico@users.noreply.github.com>
2018-08-28 15:10:49 -0500

    Página para modificar sin funcionar (aún)

--- modifi_vale.php
+++ modifi_vale.php
@@ -7,12 +7,16 @@ if (isset($id)){
    $link = mysql_connect("localhost", "root");
    mysql_select_db("mydb",$db); 
 
-   $sql = "UPDATE agenda SET nombre='$nombre', direccion='$direccion',".
-      "telefono='$telefono', email='$email' WHERE id=$id";
+   //$sql = "UPDATE agenda SET nombre='$nombre', direccion='$direccion',"."telefono='$telefono', email='$email' WHERE id=$id";
+   //$result = mysql_query($sql);
+
+   $sql = "UPDATE combustible SET vehiculo=´$vehiculo´, comision=´$comision´, kmactuales=´$kmactuales´, lugarvisita=´$lugarvisita´, asunto=´$asunto´, lts=´$lts´, fe=´$fe´, cuenta=$cuenta" WHERE id=$id";
    $result = mysql_query($sql);
+
 }else{
    echo "Debe especificar un 'id'.\n";
 }
 
 </body>
 </html> 
+
